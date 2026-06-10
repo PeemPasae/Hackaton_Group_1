31b1d8a9237a7b09ac66be399752dea19c13a610
GunCodarith <guncodarith@gmail.com>
2026-06-10 20:34:10 +0700

    เป็น script สามารถทำ autorunได้

---
+++ ถอดรหัสhackathon1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 1. คำสั่งหาไฟล์แรกที่ซ่อนอยู่ใน home
+# find /home -name "hackathon#1"
+
+# 2. คำสั่งยิงเลเซอร์ถอดรหัสข้อความ GOEMON แยกตามวัน
+grep "197.82.237.190" ../streets/locked/cart_web.log | awk -F '|' '{
+    split($1, date, " ");
+    path = $4; gsub(/ /, "", path); gsub(/\.html/, "", path);
+    last_char = substr(path, length(path));
+    if (last_char ~ /[A-Z_]/) print date[1], last_char;
+}' | sort -u
